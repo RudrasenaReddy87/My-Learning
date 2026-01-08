cbd3b5d6cd40b2d76a7257681307bb1e671ac2a3
Rudrasena Reddy <b.rudrasenareddy@gmail.com>
2026-01-08 18:18:53 +0530

    Notes updated and revised

--- My-DSA-Prep/DSA-Revision-Codes/rough.py
+++ My-DSA-Prep/DSA-Revision-Codes/rough.py
@@ -24,7 +24,31 @@ class Linkedlist:
         new.next = cur
         self.head = new
     
-    def insert_end 
+    def insert_end(self,data):
+        new = Node(data)
+        if self.head is None:
+            self.head = new
+            return 
+        cur = self.head
+        while cur.next:
+            cur = cur.next
+        
+        cur.next = new
+    
+    def insert_pos(self, pos, data):
+        new = Node(data)
+        c = 0
+        cur = self.head
+        while cur:
+            if c == pos-1:
+                new.next = cur.next
+                print(cur.data)
+                cur.next = new
+                
+            c += 1
+            cur = cur.next
+        
+
     
     def insert(self,data):
         new = Node(data)
@@ -35,6 +59,35 @@ class Linkedlist:
         cur = self.head
         cur.next = new
     
+
+    def delete_begining(self):
+        if self.head is None:
+            return 
+        cur = self.head
+        cur = cur.next
+        self.head = cur
+    
+    def delete_end(self):
+        # lets take list has more values so do delete at end
+        cur = self.head
+        while cur.next.next:
+            print(cur.data)
+            cur = cur.next
+        cur.next = None
+    
+    def delete_at_pos(self,pos):
+
+        cur = self.head
+        if pos == 0:
+            self.head = cur.next
+            return 
+        cur = self.head
+        c = 0
+        while cur:
+            if c==pos-1:
+                cur.next=cur.next.next
+            c += 1
+            cur = cur.next
     def println(self):
         cur = self.head
         itr = ''
@@ -47,7 +100,15 @@ class Linkedlist:
 lst = Linkedlist()
 lst.insert(10)
 lst.insert(20)
+lst.insert_end(30)
 lst.insert_begining(50)
 lst.insert_begining(100)
+lst.insert_end(200)
+lst.insert_pos(2,80)
+
+print(lst.println())
+lst.delete_at_pos(0)
+
+
 print(lst.println())
           
